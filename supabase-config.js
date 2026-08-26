ed73eb3ed4e4501488062521882d7fbe7fbc11a7
Aether <rs4710502@gmail.com>
2026-08-26 16:48:05 +0530

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 
 // Cloudflare Worker URL
 window.GB_SUPABASE_URL =
-  'https://grameen-bazar-api.rs4710502.workers.dev';
+  'https://grameen-bazar-api.rs4710502.workers.dev/';
 
 // Supabase Publishable/Anon key
 window.GB_SUPABASE_ANON_KEY =
